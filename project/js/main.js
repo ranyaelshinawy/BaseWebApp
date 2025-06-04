11ba8fc7254c5dde77fb5df074d864efebce8901
ranyaelshinawy <ranyaelshinawy@gmail.com>
2025-06-04 18:01:39 -0500

    Added weather api

--- project/js/main.js
+++ project/js/main.js
@@ -7,3 +7,17 @@ function showPicture(){
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
 }
+
+$(document).ready(function() {
+  getWeather();
+})
+
+function getWeather() {
+  var url = "https://api.openweathermap.org/data/2.5/weather?lat=30.2672&lon=-97.7431&units=imperial&appid="+apiKey
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text("Temp: " + data.main.temp);
+
+  }})
+}
